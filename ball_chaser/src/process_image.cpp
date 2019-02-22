ac2351491a00f6c739a8fd37f4b360734a4985e8
Tim Abrahamsen <timmy_neutron@mac.com>
2019-02-22 22:21:59 +0000

    feat: Make process_image loop through every third array index
    Make process_image loop through each pixel, which means
    every third array index (and check the red, green, and blue
    channels for each pixel)

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -23,12 +23,16 @@ void process_image_callback(const sensor_msgs::Image img)
   // Then, identify if this pixel falls in the left, mid, or right side of the image
   // Depending on the white ball position, call the drive_bot function and pass velocities to it
   // Request a stop when there's no white ball seen by the camera
-  bool ball_found = false;
 
+  // Since the ball is level with the camera, only have to scan the middle third of the image
   int scan_start = img.data.size() / 3;
   int scan_end = img.data.size() * 2 / 3;
 
-  for (int i=scan_start; i+2<scan_end; i++) {
+  bool ball_found = false;
+
+  // Scan each pixel looking for a white one
+  for (int i=scan_start; i+2<scan_end; i+=3) {
+
     int red_channel = img.data[i];
     int green_channel = img.data[i+1];
     int blue_channel = img.data[i+2];
@@ -36,8 +40,8 @@ void process_image_callback(const sensor_msgs::Image img)
     if (red_channel == 255 && green_channel == 255 && blue_channel == 255)
     {
       ball_found = true;
-
       int x_position = (i % (img.width * 3)) / 3;
+
       if (x_position < img.width / 3)
       {
         drive_robot(0.5, 0.5);
@@ -68,7 +72,8 @@ int main(int argc, char** argv)
   // Define a client service capable of requesting services from command_robot
   client = n.serviceClient<ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
 
-  // Subscribe to /camera/rgb/image_raw topic to read the image data inside the process_image_callback function
+  // Subscribe to /camera/rgb/image_raw topic to read the image data inside the
+  // process_image_callback function
   ros::Subscriber sub1 = n.subscribe("/camera/rgb/image_raw", 10, process_image_callback);
 
   // Handle ROS communication events
