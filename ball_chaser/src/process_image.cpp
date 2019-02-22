bdf3de3d0335e6cd99a3e3ca31ad5c12bd749917
Tim Abrahamsen <timmy_neutron@mac.com>
2019-02-22 22:13:20 +0000

    feat: Make robot chase white ball
    Make the robot chase the white ball based on where in the camera
    image the ball is located.

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -19,44 +19,48 @@ void drive_robot(float lin_x, float ang_z)
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
-  int white_pixel = 255;
-
   // TODO: Loop through each pixel in the image and check if there's a bright white one
   // Then, identify if this pixel falls in the left, mid, or right side of the image
   // Depending on the white ball position, call the drive_bot function and pass velocities to it
   // Request a stop when there's no white ball seen by the camera
   bool ball_found = false;
 
-  for (int i=0; i+2<img.data.size(); i++) {
-    if ((int)img.data[i] == white_pixel && (int)img.data[i+1] == white_pixel && (int)img.data[i+2] == white_pixel)
+  int scan_start = img.data.size() / 3;
+  int scan_end = img.data.size() * 2 / 3;
+
+  for (int i=scan_start; i+2<scan_end; i++) {
+    int red_channel = img.data[i];
+    int green_channel = img.data[i+1];
+    int blue_channel = img.data[i+2];
+
+    if (red_channel == 255 && green_channel == 255 && blue_channel == 255)
     {
       ball_found = true;
+
       int x_position = (i % (img.width * 3)) / 3;
       if (x_position < img.width / 3)
       {
-        ROS_INFO("BALL IS LEFT");
+        drive_robot(0.5, 0.5);
       }
       else if (x_position > img.width * 2 / 3)
       {
-        ROS_INFO("BALL IS RIGHT");
+        drive_robot(0.5, -0.5);
       }
       else
       {
-        ROS_INFO("BALL IS CENTER");
+        drive_robot(0.5, 0.0);
       }
     } 
   }
 
   if (!ball_found)
   {
-    ROS_INFO("BALL NOT FOUND");
+    drive_robot(0.0, 0.0);
   }
 }
 
 int main(int argc, char** argv)
 {
-  ROS_INFO("Hello world!");
-
   // Initialize the process_image node and create a handle to it
   ros::init(argc, argv, "process_image");
   ros::NodeHandle n;
